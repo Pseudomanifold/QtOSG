edb699227a4584e67d25ef8db4c5ec8795709d4f
Bastian Rieck <bastian@rieck.ru>
2017-06-28 08:19:24 +0200

    Setting default surface format
    As suggested by the principal developer of OpenScenegGraph in the
    following thread
    
      https://groups.google.com/forum/#!msg/osg-users/A89jTUGq4VY/RsmqvF1JAwAJ
    
    I am now setting a default profile for the Qt application to force usage
    of OpenGL 2.1. I am not sure whether this may solve issue #4, but let's
    give it a try.

--- QtOSG.cpp
+++ QtOSG.cpp
@@ -1,10 +1,17 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <QSurfaceFormat>
 
 int main( int argc, char** argv )
 {
   QApplication application( argc, argv );
+
+  QSurfaceFormat format;
+  format.setVersion(2, 1);
+  format.setProfile( QSurfaceFormat::CompatibilityProfile );
+
+  QSurfaceFormat::setDefaultFormat(format);
 
   MainWindow mainWindow;
   mainWindow.show();
